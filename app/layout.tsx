48bfd8b94ace0263e299d3d0caefbd35294e8625
Vibhashana <svbbk.92@gmail.com>
2024-12-28 00:16:21 +0530

    Added authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "@/app/ui/global.css";
 import { poppins } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Acme Dashboard",
+  description: "A dashboard created with Next.js App Router",
+};
 
 export default function RootLayout({
   children,
