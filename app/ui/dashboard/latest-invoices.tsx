77055485a9a46f2b0c57e44791455d02a05d0c36
Vibhashana <svbbk.92@gmail.com>
2024-12-28 22:55:45 +0530

    Updated the UI on the dashboard page

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,55 +3,67 @@ import clsx from "clsx";
 import Image from "next/image";
 import { LatestInvoice } from "@/app/lib/definitions";
 import { fetchLatestInvoices } from "@/app/lib/data";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default async function LatestInvoices() {
   const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className="mb-4 text-xl md:text-2xl">Latest Invoices</h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        <div className="bg-white px-6">
-          {latestInvoices.map((invoice, i) => {
-            return (
-              <div
-                key={invoice.id}
-                className={clsx(
-                  "flex flex-row items-center justify-between py-4",
-                  {
-                    "border-t": i !== 0,
-                  }
-                )}
-              >
-                <div className="flex items-center">
-                  <Image
-                    src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
-                    className="mr-4 rounded-full"
-                    width={32}
-                    height={32}
-                  />
-                  <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
-                      {invoice.name}
-                    </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
-                      {invoice.email}
-                    </p>
+      <Card className="h-full">
+        <CardHeader>
+          <CardTitle>Latest Invoices</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex grow flex-col justify-between rounded-md bg-primary-foreground dark:bg-primary-foreground/75 p-4">
+            <div className="">
+              {latestInvoices.map((invoice, i) => {
+                return (
+                  <div
+                    key={invoice.id}
+                    className="flex flex-row items-center justify-between py-4"
+                  >
+                    <div className="flex items-center gap-4">
+                      <Avatar>
+                        <AvatarImage
+                          src={invoice.image_url}
+                          alt={`${invoice.name}'s profile picture`}
+                        />
+                        <AvatarFallback>
+                          {invoice.name.charAt(0)}
+                        </AvatarFallback>
+                      </Avatar>
+
+                      <div className="min-w-0 space-y-1">
+                        <p className="truncate text-sm font-medium">
+                          {invoice.name}
+                        </p>
+                        <p className="hidden text-xs text-muted-foreground sm:block">
+                          {invoice.email}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="truncate font-medium">{invoice.amount}</p>
                   </div>
-                </div>
-                <p className="truncate text-sm font-medium md:text-base">
-                  {invoice.amount}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-        <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
-        </div>
-      </div>
+                );
+              })}
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center text-muted-foreground">
+            <ArrowPathIcon className="h-5 w-5" />
+            <h3 className="ml-2 text-sm">Updated just now</h3>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -3,6 +3,13 @@ import { CalendarIcon } from "@heroicons/react/24/outline";
 import { Revenue } from "@/app/lib/definitions";
 
 import { fetchRevenue } from "@/app/lib/data";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -23,38 +30,46 @@ export default async function RevenueChart() {
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className="mb-4 text-xl md:text-2xl">Recent Revenue</h2>
+      <Card className="h-full">
+        <CardHeader>
+          <CardTitle>Recent Revenue</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-primary-foreground p-4 md:gap-4">
+            <div
+              className="mb-6 hidden flex-col justify-between text-sm text-muted-foreground sm:flex"
+              style={{ height: `${chartHeight}px` }}
+            >
+              {yAxisLabels.map((label) => (
+                <p key={label}>{label}</p>
+              ))}
+            </div>
 
-      <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
-          >
-            {yAxisLabels.map((label) => (
-              <p key={label}>{label}</p>
+            {revenue.map((month) => (
+              <div
+                key={month.month}
+                className="flex flex-col items-center gap-2"
+              >
+                <div
+                  className="w-full rounded-md bg-blue-400"
+                  style={{
+                    height: `${(chartHeight / topLabel) * month.revenue}px`,
+                  }}
+                ></div>
+                <p className="-rotate-90 text-sm text-muted-foreground sm:rotate-0">
+                  {month.month}
+                </p>
+              </div>
             ))}
           </div>
-
-          {revenue.map((month) => (
-            <div key={month.month} className="flex flex-col items-center gap-2">
-              <div
-                className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
-              ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-                {month.month}
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
-        </div>
-      </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center text-muted-foreground">
+            <CalendarIcon className="h-5 w-5" />
+            <span className="ml-2 text-sm">Last 12 months</span>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
